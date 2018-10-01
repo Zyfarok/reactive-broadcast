ffee69cbd92421effa0401e72424153d5252f341
Pierre Thévenet <thevenet.pierre@gmail.com>
2018-10-01 10:14:45 +0200

    INT+TERM signals handling

--- da_project_2018/src/da_project_2018/Main.java
+++ da_project_2018/src/da_project_2018/Main.java
@@ -1,29 +1,58 @@
 package da_project_2018;
 
-import signal.*;
+import sun.misc.Signal;
+import sun.misc.SignalHandler;
 import logging.Logging;
 import java.util.logging.Logger;
 import java.lang.*;
+import java.nio.channels.ClosedByInterruptException;
+
+import da_project_2018.*;
 
 public class Main {
+	
+	public static class MyThread extends Thread {
+		private String name;
+		
+		public MyThread(String name) {
+			super();
+			this.name = name;
+		}
+		
+		public void run() {
+			while (true) {
+				try {
+					System.out.println("thread " + this.name + " running");				
+					Thread.sleep(1000);
+				} catch (InterruptedException e) {
+					return;
+				} catch (Exception e) {
+					System.out.println(e);
+					return;
+				}
+			}
+		}
+	}
 
 	public static void main(String[] args) {		
 		
 		Logging.log("da_proc: running");
 		
-		signal.StopSignalHandler.install("TERM");
-		signal.StopSignalHandler.install("INT");
+		Thread[] threads = {
+			new Main.MyThread("1"),
+			new Main.MyThread("2")
+		};
 		
-		while (true) {
-			
-			try {
-				System.out.println("running...");
-				Thread.sleep(1000);
-			} catch (Exception e) {
-				System.out.println("main: exception: " + e);
+		 Runtime.getRuntime().addShutdownHook(new ShutdownThread(threads));
 				
-			}
+		
+		for (int i = 0; i < threads.length; i++) {
+			Thread t = threads[i];
+			t.start();
 		}
+		
+		return;
 	}
+	
 
 }
---
+++ da_project_2018/src/da_project_2018/ShutdownThread.java
@@ -0,0 +1,38 @@
+package da_project_2018;
+
+import logging.Logging;
+
+public class ShutdownThread extends Thread {
+	private final Thread[] threads;
+	
+	/*
+	 * Initializes a ShutdownThread hook.
+	 * @param 	threads		array of Thread to be interrupted once stop signal has been received
+	 */
+	public ShutdownThread(Thread[] threads) {
+		int N = threads.length;
+		this.threads = new Thread[N];
+		for (int i = 0; i < N; i++) {
+			this.threads[i] = threads[i];
+		}
+	}
+	
+	/*
+	 * Logic executed after a stop signal is received, and before the JVM actual stop.
+	 * @see java.lang.Thread#run()
+	 */
+	public void run()  {
+		Logging.log("shutting down...");
+		
+		// stop threads
+		for (Thread t: this.threads) {
+			t.interrupt();
+		}
+		
+		// flush logs
+		Logging.flush();
+		
+		return;
+	}
+}
+	
