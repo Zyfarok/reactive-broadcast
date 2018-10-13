6ca1a75948a9144cc599336e402c1bc782ac2126
Zyfarok <zyfarok@gmail.com>
2018-10-14 01:34:29 +0200

    Redifine RxSocket to use Subjects to simplify things and apply the necessary changes to RxUDPSocket + merge stuff in it

--- src/main/java/ch/epfl/daeasy/rxsockets/RxSocket.java
+++ src/main/java/ch/epfl/daeasy/rxsockets/RxSocket.java
@@ -1,16 +1,17 @@
 package ch.epfl.daeasy.rxsockets;
 
 import io.reactivex.Observable;
+import io.reactivex.subjects.PublishSubject;
 
 import ch.epfl.daeasy.rxlayers.RxLayer;
 
 public class RxSocket<A> {
     public final Observable<A> inputPipe;
-    public final RxOutputBuilder<A> outputBuilder;
+    public final PublishSubject<A> outputPipe;
 
-    public RxSocket(final Observable<A> in, final RxOutputBuilder<A> out) {
+    public RxSocket(final Observable<A> in, final PublishSubject<A> out) {
         this.inputPipe = in;
-        this.outputBuilder = out;
+        this.outputPipe = out;
     }
 
     public <B> RxSocket<B> stack(RxLayer<B,A> layer) {
--- src/main/java/ch/epfl/daeasy/rxsockets/RxUDPSocket.java
+++ src/main/java/ch/epfl/daeasy/rxsockets/RxUDPSocket.java
@@ -1,10 +1,71 @@
 package ch.epfl.daeasy.rxsockets;
 
+import io.reactivex.Observable;
+import io.reactivex.ObservableEmitter;
+import io.reactivex.ObservableOnSubscribe;
+import io.reactivex.disposables.Disposable;
+import io.reactivex.functions.Cancellable;
+import io.reactivex.schedulers.Schedulers;
+import io.reactivex.subjects.PublishSubject;
+
+import javax.security.auth.Subject;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 
 public class RxUDPSocket extends RxSocket<DatagramPacket> {
-    public RxUDPSocket(final DatagramSocket udpSocket) {
-        super(RxUDPInputCreator.create(udpSocket), new RxUDPOutputBuilder(udpSocket));
+    public RxUDPSocket(DatagramSocket udpSocket) {
+        this(udpSocket, createUDPOuputSubject(udpSocket));
+    }
+
+    private RxUDPSocket(DatagramSocket udpSocket, SubjectAndItsDisposable said) {
+        super(createUDPInputObserver(udpSocket, said.disposable), said.subject);
+    }
+
+    private static Cancellable createCancellable(DatagramSocket udpSocket, Disposable disposable) {
+        return new Cancellable() {
+            @Override
+            public void cancel() throws Exception {
+                disposable.dispose();
+                if (!udpSocket.isClosed()) {
+                    udpSocket.close();
+                }
+            }
+        };
+    }
+
+    private static Observable<DatagramPacket> createUDPInputObserver(DatagramSocket udpSocket, Disposable disposable) {
+        return Observable.create(
+                new ObservableOnSubscribe<DatagramPacket>() {
+                    @Override
+                    public void subscribe(ObservableEmitter<DatagramPacket> emitter) throws Exception {
+                        emitter.setCancellable(createCancellable(udpSocket, disposable));
+                        while (true) {
+                            try {
+                                byte[] rcvBuffer = new byte[1000000]; // TODO : Define the array size in an a better fashion.
+                                DatagramPacket datagramPacket = new DatagramPacket(rcvBuffer, rcvBuffer.length);
+                                udpSocket.receive(datagramPacket);
+                                emitter.onNext(datagramPacket);
+                            } catch (Exception e) {
+                                emitter.onError(e);
+                            }
+                        }
+                    }
+                }
+        ).subscribeOn(Schedulers.io());
+    }
+
+    private static SubjectAndItsDisposable createUDPOuputSubject(final DatagramSocket udpSocket) {
+        PublishSubject<DatagramPacket> subject = PublishSubject.create();
+        return new SubjectAndItsDisposable(subject, subject.forEach(udpSocket::send));
+    }
+
+    private static class SubjectAndItsDisposable {
+        final PublishSubject<DatagramPacket> subject;
+        final Disposable disposable;
+
+        private SubjectAndItsDisposable(PublishSubject<DatagramPacket> subject, Disposable disposable) {
+            this.subject = subject;
+            this.disposable = disposable;
+        }
     }
 }
