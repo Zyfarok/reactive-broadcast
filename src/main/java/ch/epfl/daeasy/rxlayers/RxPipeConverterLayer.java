76077b83cd9f6fb1c596ea72e5287faa855dc936
Zyfarok <zyfarok@gmail.com>
2018-10-14 01:44:33 +0200

    Change RxPipeConverter to comply with the use of subjects in the new RxSocket implementation

--- src/main/java/ch/epfl/daeasy/rxlayers/RxPipeConverterLayer.java
+++ src/main/java/ch/epfl/daeasy/rxlayers/RxPipeConverterLayer.java
@@ -1,21 +1,56 @@
 package ch.epfl.daeasy.rxlayers;
 
+import javax.annotation.Nonnull;
+
 import com.google.common.base.Converter;
 
 import io.reactivex.Observable;
+import io.reactivex.functions.Function;
+import io.reactivex.observables.GroupedObservable;
+import io.reactivex.subjects.PublishSubject;
 
 import ch.epfl.daeasy.rxsockets.RxSocket;
 
 public class RxPipeConverterLayer<A,B> extends RxLayer<A,B> {
-    final private Converter<Observable<B>,Observable<A>> converter;
+    private final Converter<Observable<B>,Observable<A>> converter;
     public RxPipeConverterLayer(final Converter<Observable<B>,Observable<A>> bottomUpConverter) {
         this.converter = bottomUpConverter;
     }
 
     public RxSocket<A> stackOn(final RxSocket<B> subSocket) {
-        return new RxSocket<A>(
-            converter.convert(subSocket.inputPipe),
-            subSocket.outputBuilder.transform(converter.reverse())
-        );
+        PublishSubject<A> subject = PublishSubject.create();
+        converter.reverse().convert(subject).subscribe(subSocket.outputPipe);
+        return new RxSocket<A>(converter.convert(subSocket.inputPipe), subject);
+    }
+
+    public RxPipeConverterLayer<B,A> reverse() {
+        return new RxPipeConverterLayer<B,A>(converter.reverse());
+    }
+
+    public static <K,A> RxPipeConverterLayer<GroupedObservable<K,A>,A> mux(Function<A,K> key) {
+        return new RxPipeConverterLayer<GroupedObservable<K,A>,A>(new MuxConverter<K,A>(key));
+    }
+
+    public static <K,A> RxPipeConverterLayer<A,GroupedObservable<K,A>> demux(Function<A,K> key) {
+        return mux(key).reverse();
+    }
+
+    private static class MuxConverter<K,A> extends Converter<Observable<A>, Observable<GroupedObservable<K, A>>> {
+        private final Function<A,K> key;
+        MuxConverter(Function<A,K> key) {
+            this.key = key;
+        }
+
+        @Override
+        protected Observable<GroupedObservable<K, A>> doForward(@Nonnull Observable<A> inputPipe) {
+            return inputPipe.groupBy(key);
+        }
+
+        @Override
+        protected Observable<A> doBackward(@Nonnull Observable<GroupedObservable<K, A>> groupedOuputPipe) {
+            return groupedOuputPipe.flatMap(group -> group);
+        }
     }
 }
+
+
