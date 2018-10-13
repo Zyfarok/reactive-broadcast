66dd30007a22b80bde1c824547cfc620436a4e84
Zyfarok <zyfarok@gmail.com>
2018-10-13 15:19:41 +0200

    Replace getters by public variables and add stack method to RxSocket

--- src/main/java/ch/epfl/daeasy/rxsockets/RxSocket.java
+++ src/main/java/ch/epfl/daeasy/rxsockets/RxSocket.java
@@ -1,29 +1,19 @@
 package ch.epfl.daeasy.rxsockets;
 
-import java.util.function.Function;
-
 import io.reactivex.Observable;
 
+import ch.epfl.daeasy.layers.RxLayer;
+
 public class RxSocket<A> {
-    private final Observable<A> subInputPipe;
-    private final RxOutputBuilder<A> subOutputBuilder;
+    public final Observable<A> inputPipe;
+    public final RxOutputBuilder<A> outputBuilder;
 
-    public RxSocket(final Observable<A> sin, final RxOutputBuilder<A> sob) {
-        this.subInputPipe = sin;
-        this.subOutputBuilder = sob;
+    public RxSocket(final Observable<A> in, final RxOutputBuilder<A> out) {
+        this.inputPipe = in;
+        this.outputBuilder = out;
     }
 
-    public <B> RxSocket(final RxSocket<B> subSocket,
-            final Function<Observable<B>,Observable<A>> inConverter,
-            final Function<Observable<A>,Observable<B>> outConverter) {
-        this(inConverter.apply(subSocket.getInputPipe()), subSocket.getOutputBuilder().transform(outConverter));
-    }
-
-    public Observable<A> getInputPipe() {
-        return subInputPipe;
-    }
-    
-    public RxOutputBuilder<A> getOutputBuilder() {
-        return subOutputBuilder;
+    public <B> RxSocket<B> stack(RxLayer<B,A> layer) {
+        return layer.stackOn(this);
     }
 }
