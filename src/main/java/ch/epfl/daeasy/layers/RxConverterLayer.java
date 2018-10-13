5ceb9f13c2c4250f94529ae3f783dfe348a8fe53
Zyfarok <zyfarok@gmail.com>
2018-10-13 16:13:10 +0200

    Implement RxConverterLayer

---
+++ src/main/java/ch/epfl/daeasy/layers/RxConverterLayer.java
@@ -0,0 +1,21 @@
+package ch.epfl.daeasy.layers;
+
+import com.google.common.base.Converter;
+
+import io.reactivex.Observable;
+
+import ch.epfl.daeasy.rxsockets.RxSocket;
+
+public class RxConverterLayer<A,B> extends RxLayer<A,B> {
+    final private Converter<Observable<B>,Observable<A>> converter;
+    public RxConverterLayer(final Converter<Observable<B>,Observable<A>> converter) {
+        this.converter = converter;
+    }
+
+    public RxSocket<A> stackOn(final RxSocket<B> subSocket) {
+        return new RxSocket<A>(
+            converter.convert(subSocket.inputPipe),
+            subSocket.outputBuilder.transform(converter.reverse())
+        );
+    }
+}
