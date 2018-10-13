dadd5039bfd96000db14e1c27e1673349f0186cc
Zyfarok <zyfarok@gmail.com>
2018-10-13 15:21:09 +0200

    Change RxLayer to abstract, remove constructor and add stackOn and stack methods

--- src/main/java/ch/epfl/daeasy/layers/RxLayer.java
+++ src/main/java/ch/epfl/daeasy/layers/RxLayer.java
@@ -1,32 +1,27 @@
 package ch.epfl.daeasy.layers;
 
-import java.util.function.Function;
-
-import io.reactivex.Observable;
-
 import ch.epfl.daeasy.rxsockets.RxSocket;
+
 /**
  * This is a RxSocket "builder".
  * 
  * @param <A> type of RxSocket that it builds.
  * @param <B> type of RxSocket that it stacks onto.
  */
-public class RxLayer<A,B> {
-    private final Function<Observable<B>,Observable<A>> inConverter;
-    private final Function<Observable<A>,Observable<B>> outConverter;
-
-    public RxLayer(final Function<Observable<B>,Observable<A>> inConverter,
-            final Function<Observable<A>,Observable<B>> outConverter) {
-        this.inConverter = inConverter;
-        this.outConverter = outConverter;
-    }
-
-    public RxSocket<A> stackOn(final RxSocket<B> subSocket) {
-        return new RxSocket(subSocket, inConverter, outConverter);
-    }
+public abstract class RxLayer<A,B> {
+    public abstract RxSocket<A> stackOn(final RxSocket<B> subSocket);
 
     public <C> RxLayer<A,C> stackOn(final RxLayer<B,C> that) {
-        return new RxLayer<A,C>(that.inConverter.andThen(this.inConverter),
-            this.outConverter.andThen(that.outConverter));
+        RxLayer<A,B> it = this;
+        return new RxLayer<A,C>() {
+            @Override
+            public RxSocket<A> stackOn(final RxSocket<C> subSocket) {
+                return subSocket.stack(that).stack(it);
+            }
+        };
+    }
+
+    public <C> RxLayer<C,B> stack(final RxLayer<C,A> that) {
+        return that.stackOn(this);
     }
 }
