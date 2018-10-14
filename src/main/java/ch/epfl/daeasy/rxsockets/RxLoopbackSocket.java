b7ea1b58e058e505902dccb6046db99cd3ab2ead
Zyfarok <zyfarok@gmail.com>
2018-10-14 21:22:44 +0200

    Fix RxLoopbackSocket subscription

--- src/main/java/ch/epfl/daeasy/rxsockets/RxLoopbackSocket.java
+++ src/main/java/ch/epfl/daeasy/rxsockets/RxLoopbackSocket.java
@@ -3,9 +3,9 @@ package ch.epfl.daeasy.rxsockets;
 import io.reactivex.subjects.PublishSubject;
 
 public class RxLoopbackSocket<A> extends RxSocket<A> {
-    public RxLoopbackSocket(PublishSubject<A> loopbackSubject) {
+    private RxLoopbackSocket(PublishSubject<A> loopbackSubject) {
         super(loopbackSubject);
-        loopbackSubject.subscribe(this.outputPipe);
+        this.outputPipe.subscribe(loopbackSubject);
     }
 
     public RxLoopbackSocket() {
