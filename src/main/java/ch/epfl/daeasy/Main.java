32ba011336bcc19f0471752944ea4aef614ca20e
Zyfarok <zyfarok@gmail.com>
2018-10-15 11:10:05 +0200

    Change main to RxGroupedLayer test

--- src/main/java/ch/epfl/daeasy/Main.java
+++ src/main/java/ch/epfl/daeasy/Main.java
@@ -4,18 +4,29 @@ import ch.epfl.daeasy.config.Configuration;
 import ch.epfl.daeasy.config.Process;
 import ch.epfl.daeasy.logging.Logging;
 import ch.epfl.daeasy.protocol.Message;
+import ch.epfl.daeasy.rxlayers.RxConverterLayer;
+import ch.epfl.daeasy.rxlayers.RxFilterLayer;
+import ch.epfl.daeasy.rxlayers.RxGroupedLayer;
 import ch.epfl.daeasy.rxsockets.RxLoopbackSocket;
+import ch.epfl.daeasy.rxsockets.RxSocket;
 import ch.epfl.daeasy.rxsockets.RxUDPSocket;
 import ch.epfl.daeasy.signals.StartSignalHandler;
 import ch.epfl.daeasy.signals.StopSignalHandler;
+import com.google.common.base.Converter;
 import io.reactivex.Observable;
+import io.reactivex.Observer;
 import io.reactivex.disposables.Disposable;
+import io.reactivex.functions.Predicate;
+import io.reactivex.observables.ConnectableObservable;
+import io.reactivex.observables.GroupedObservable;
+import io.reactivex.subjects.PublishSubject;
 
 import java.net.DatagramPacket;
 import java.net.InetSocketAddress;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.TimeUnit;
+import java.util.function.Function;
 
 public class Main {
 
@@ -83,31 +94,61 @@ public class Main {
 		RxUDPSocket udp = new RxUDPSocket(cfg.udpSocket);
 		*/
 		// udp.inputPipe.blockingSubscribe(stuff -> System.out.println(stuff));
-		RxLoopbackSocket<String> sock = new RxLoopbackSocket<>();
+        RxSocket<Integer> subSocket = new RxLoopbackSocket<>();
+		RxSocket<Integer> sock = subSocket.stack(
+		        RxGroupedLayer.create(x -> (x % 2),
+                        new RxConverterLayer<>(Converter.from(x -> (x+100), x -> (x-100))))
+        );
 
-		Disposable d1 = sock.inputPipe.forEach(System.out::println);
+		ConnectableObservable<Integer> o = sock.inputPipe.publish();
+        GroupedObservable<Integer,Integer> z = new GroupedObservable<Integer, Integer>(0) {
+            final PublishSubject<Integer> subject = PublishSubject.create();
 
-		sock.outputPipe.onNext("1");
-		sock.outputPipe.onNext("2");
-		sock.outputPipe.onNext("3");
+            @Override
+            protected void subscribeActual(Observer<? super Integer> observer) {
+                subject.subscribe(observer);
+            }
+        };
 
-		Disposable d2 = sock.inputPipe.forEach(s -> System.out.println(s + "bis"));
+        subSocket.outputPipe.onNext(0);
+        subSocket.outputPipe.onNext(1);
+        subSocket.outputPipe.onNext(2);
+        subSocket.outputPipe.onNext(3);
 
-		sock.outputPipe.onNext("4");
-		sock.outputPipe.onNext("5");
-		sock.outputPipe.onNext("6");
+        o.connect();
+        Disposable d1 = o.forEach(System.out::println);
 
-		d1.dispose();
+        subSocket.outputPipe.onNext(4);
+        subSocket.outputPipe.onNext(5);
+        subSocket.outputPipe.onNext(6);
+        subSocket.outputPipe.onNext(7);
 
-        sock.outputPipe.onNext("7");
-        sock.outputPipe.onNext("8");
-        sock.outputPipe.onNext("9");
+        Disposable d2 = o.forEach(s -> System.out.println(s + "bis"));
 
+        subSocket.outputPipe.onNext(8);
+        subSocket.outputPipe.onNext(9);
+        subSocket.outputPipe.onNext(10);
+        subSocket.outputPipe.onNext(11);
+
+        d1.dispose();
         d2.dispose();
+
+        subSocket.outputPipe.onNext(12);
+        subSocket.outputPipe.onNext(13);
+        subSocket.outputPipe.onNext(14);
+        subSocket.outputPipe.onNext(15);
+
+        Disposable d3 = o.forEach(s -> System.out.println(s + "ter"));
 
 		System.out.println("Done pushing.");
 
+
+        //PublishSubject<Integer> incomingOutputPipe = PublishSubject.create();
+        //Observable<GroupedObservable<K,A>> incomingGroupedOutputPipes = incomingOutputPipe.groupBy(key);
+
 		try {Thread.sleep(1000);} catch (InterruptedException ignored) {}
+
+        d3.dispose();
 	}
 
 }
