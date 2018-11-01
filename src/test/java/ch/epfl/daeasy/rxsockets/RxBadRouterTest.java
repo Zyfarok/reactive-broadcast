6f916864185beee4ad42d31e40d3bf4c8f76a0fe
Zyfarok <zyfarok@gmail.com>
2018-11-01 22:37:37 +0100

    Improve RxBadRouterTest

--- src/test/java/ch/epfl/daeasy/rxsockets/RxBadRouterTest.java
+++ src/test/java/ch/epfl/daeasy/rxsockets/RxBadRouterTest.java
@@ -1,21 +1,24 @@
 package ch.epfl.daeasy.rxsockets;
 
 import io.reactivex.observers.TestObserver;
+import org.junit.Assert;
 import org.junit.Test;
 
-import java.net.*;
-import java.util.concurrent.TimeUnit;
+import java.net.DatagramPacket;
+import java.net.InetSocketAddress;
+import java.net.SocketAddress;
 
+import static java.util.concurrent.TimeUnit.MILLISECONDS;
 import static java.util.concurrent.TimeUnit.SECONDS;
 
 public class RxBadRouterTest {
     @Test
-    public void perfectBadRouterTransmitPackets() throws UnknownHostException {
+    public void perfectBadRouterTransmitPackets() {
         // Create rooter and 2 sockets
         RxBadRouter rooter = new RxBadRouter(0,0, 0, SECONDS);
 
-        SocketAddress address1 = new InetSocketAddress(InetAddress.getLocalHost(),1000);
-        SocketAddress address2 = new InetSocketAddress(InetAddress.getLocalHost(),1001);
+        SocketAddress address1 = new InetSocketAddress("127.0.0.1",1000);
+        SocketAddress address2 = new InetSocketAddress("127.0.0.1",1001);
         RxSocket<DatagramPacket> socket1 = rooter.buildSocket(address1);
         RxSocket<DatagramPacket> socket2 = rooter.buildSocket(address2);
 
@@ -53,26 +56,22 @@ public class RxBadRouterTest {
 
 
     @Test
-    public void wostBadRouterNeverTransmitPacket() throws UnknownHostException {
+    public void worstBadRouterNeverTransmitPacket() throws InterruptedException {
         // Create rooter and 2 sockets
         RxBadRouter rooter = new RxBadRouter(1,0, 0, SECONDS);
 
-        SocketAddress address1 = new InetSocketAddress(InetAddress.getLocalHost(),1000);
-        SocketAddress address2 = new InetSocketAddress(InetAddress.getLocalHost(),1001);
+        SocketAddress address1 = new InetSocketAddress("127.0.0.1",1000);
+        SocketAddress address2 = new InetSocketAddress("127.0.0.1",1001);
         RxSocket<DatagramPacket> socket1 = rooter.buildSocket(address1);
         RxSocket<DatagramPacket> socket2 = rooter.buildSocket(address2);
 
         // Create TestObservers
-        TestObserver<String> test1To2 = socket2.upPipe
-                .map(DatagramPacket::getData).map(String::new)
-                .firstElement().test();
-        TestObserver<String> test2To1 = socket1.upPipe
-                .map(DatagramPacket::getData).map(String::new)
+        TestObserver<String> test = socket2.upPipe.mergeWith(socket1.upPipe)
+                .map(x -> new String(x.getData(), x.getOffset(), x.getLength()))
                 .firstElement().test();
 
-        // None is done yet !
-        test1To2.assertNotComplete();
-        test2To1.assertNotComplete();
+        // Nothing is done yet !
+        test.assertNotComplete();
 
         // Send message from 1 to 2
         String message1To2 = "1To2";
@@ -84,8 +83,8 @@ public class RxBadRouterTest {
         socket2.downPipe.onNext(new DatagramPacket(message2To1.getBytes(),
                 0, message2To1.getBytes().length, address1));
 
-        // None is done since packets are dropped
-        test1To2.assertNotComplete();
-        test2To1.assertNotComplete();
+        // Nothing is received since packets are dropped
+        Assert.assertFalse(test.await(50, MILLISECONDS));
+        test.assertNever("1To2").assertNever("2To1").assertNotComplete();
     }
 }
