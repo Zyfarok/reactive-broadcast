91625c1ce8eaf79a74ee5b450a0d98cd8fd5e100
Zyfarok <zyfarok@gmail.com>
2018-11-01 22:38:22 +0100

    Small improve of DatagramPacketConverterTest

--- src/test/java/ch/epfl/daeasy/protocol/DatagramPacketConverterTest.java
+++ src/test/java/ch/epfl/daeasy/protocol/DatagramPacketConverterTest.java
@@ -1,25 +1,24 @@
 package ch.epfl.daeasy.protocol;
 
+import io.reactivex.Observable;
+import io.reactivex.observers.TestObserver;
+import org.junit.Test;
+
 import java.net.InetSocketAddress;
 import java.net.SocketAddress;
 
-import org.junit.Test;
-
-import io.reactivex.Observable;
-import io.reactivex.observers.TestObserver;
-
 public class DatagramPacketConverterTest {
 
-    DatagramPacketConverter converter = new DatagramPacketConverter();
+    private DatagramPacketConverter converter = new DatagramPacketConverter();
 
     @Test
     public void identityTest() {
         // test that converting a DAPacket to a DatagramPacket and then back to a
         // DAPacket is identity
-        SocketAddress peer = new InetSocketAddress("localhost", 10000);
+        SocketAddress peer = new InetSocketAddress("127.0.0.1", 10000);
         MessageContent content = MessageContent.Message(1, 42);
         DAPacket msg = new DAPacket(peer, content);
-        Observable<DAPacket> msgs = Observable.fromArray(new DAPacket[] { msg });
+        Observable<DAPacket> msgs = Observable.fromArray(msg);
         TestObserver<DAPacket> msgs2 = converter.doForward(converter.doBackward(msgs)).test();
         msgs2.assertResult(msg);
     }
