b15cf516b2bcde458d0779e1589847075badf52d
Zyfarok <zyfarok@gmail.com>
2018-11-02 02:33:23 +0100

    Highly simplify PerfectLinkLayer

--- src/main/java/ch/epfl/daeasy/layers/PerfectLinkLayer.java
+++ src/main/java/ch/epfl/daeasy/layers/PerfectLinkLayer.java
@@ -10,55 +10,34 @@ import ch.epfl.daeasy.protocol.MessageContent;
 import ch.epfl.daeasy.rxlayers.RxLayer;
 import ch.epfl.daeasy.rxsockets.RxSocket;
 import io.reactivex.Observable;
+import io.reactivex.functions.Predicate;
 import io.reactivex.subjects.PublishSubject;
 import io.reactivex.subjects.Subject;
 
 public class PerfectLinkLayer extends RxLayer<DAPacket, DAPacket> {
 
     public RxSocket<DAPacket> stackOn(RxSocket<DAPacket> subSocket) {
+        // Incoming Messages
+        Observable<DAPacket> messagesUp = subSocket.upPipe.filter(pkt -> pkt.getContent().isMessage());
+        // Incoming ACK
+        Observable<DAPacket> acksUp = subSocket.upPipe.filter(pkt -> pkt.getContent().isACK());
 
-        // contains every message ID previously acked
-        Set<Long> acked = new HashSet<>();
-
-        Subject<DAPacket> subject = PublishSubject.create();
-
-        RxSocket<DAPacket> socket = new RxSocket<>(subject);
-
-        Subject<DAPacket> intOut = subject;
-        Observable<DAPacket> intIn = socket.downPipe;
-        Observable<DAPacket> extIn = subSocket.upPipe;
-        Subject<DAPacket> extOut = subSocket.downPipe;
-
-        // Exterior Messages
-        Observable<DAPacket> messagesExt = extIn.filter(pkt -> pkt.getContent().isMessage());
-        // Exterior ACKs
-        Observable<DAPacket> acksExt = extIn.filter(pkt -> pkt.getContent().isACK());
-
-        // Interior Messages
-        Observable<DAPacket> messagesIn = intIn;
-        // No Interior ACKs
+        RxSocket<DAPacket> socket = new RxSocket<>(messagesUp.distinct());
 
         // Transform ack messages to simple long stream
-        Observable<Long> acks = acksExt.map(ack -> ack.getContent().getAck().get());
+        Observable<Long> acks = acksUp.map(ack -> ack.getContent().getAck().get());
 
-        // add the acks to acked
-        acks.subscribe(ack -> acked.add(ack));
+        // Send ACK for all received messages
+        messagesUp.map(msg -> new DAPacket(msg.getPeer(), MessageContent.ackFromMessage(msg.getContent())))
+                .subscribe(subSocket.downPipe);
 
-        // ACK all received messages
-        Observable<DAPacket> acksFromMessages = messagesExt
-                .map(msg -> new DAPacket(msg.getPeer(), MessageContent.ackFromMessage(msg.getContent())));
-
-        Observable<DAPacket> messagesOut = messagesIn.map(msg -> {
-            return Observable.just(msg).repeatWhen(completed -> completed.delay(50, TimeUnit.MILLISECONDS))
-                    .filter(a -> !acked.contains(msg.getContent().getSeq().get()));
-        }).flatMap(x -> x);
-
-        // Send to Ext
-        acksFromMessages.subscribe(extOut); // acks
-        messagesOut.subscribe(extOut); // and messages to retransmit
-
-        // Send to Int
-        messagesExt.distinct().subscribe(intOut);
+        // Replay down-going messages until acked
+        socket.downPipe.map(p -> {
+            Long id = p.getContent().getSeq().get();
+            return Observable.just(p)
+                            .repeatWhen(completed -> completed.delay(50, TimeUnit.MILLISECONDS))
+                            .takeUntil(acks.filter(a -> a.equals(id)));
+        }).flatMap(o -> o).subscribe(subSocket.downPipe);
 
         return socket;
     }
