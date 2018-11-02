003730ad2694d29b242b310849cdca691d0d1fd0
Zyfarok <zyfarok@gmail.com>
2018-11-02 02:32:30 +0100

    Improve Perfect Link test with more messages sent

--- src/test/java/ch/epfl/daeasy/layers/PerfectLinkLayerTest.java
+++ src/test/java/ch/epfl/daeasy/layers/PerfectLinkLayerTest.java
@@ -7,7 +7,9 @@ import ch.epfl.daeasy.rxlayers.RxLayer;
 import ch.epfl.daeasy.rxlayers.RxNil;
 import ch.epfl.daeasy.rxsockets.RxBadRouter;
 import ch.epfl.daeasy.rxsockets.RxSocket;
+import io.reactivex.Observable;
 import io.reactivex.observers.TestObserver;
+import io.reactivex.schedulers.Schedulers;
 import org.junit.Test;
 
 import java.net.DatagramPacket;
@@ -16,38 +18,42 @@ import java.net.SocketAddress;
 import java.util.*;
 import java.util.concurrent.TimeUnit;
 import java.util.stream.Collectors;
-import java.util.stream.Stream;
+import java.util.stream.IntStream;
 
 public class PerfectLinkLayerTest {
     private final DatagramPacketConverter daConverter = new DatagramPacketConverter();
     private final RxLayer<DatagramPacket,DAPacket> layers = new RxNil<DatagramPacket>()
-            .convertPipes(daConverter).stack(new PerfectLinkLayer());
+            .scheduleOn(Schedulers.trampoline())
+            .convertPipes(daConverter)
+            .stack(new PerfectLinkLayer());
     @Test
     public void perfectLinkEndsUpSendingPackets() {
-        RxBadRouter router = new RxBadRouter(0.9,0.9,0, TimeUnit.NANOSECONDS);
+        RxBadRouter router = new RxBadRouter(0.2,0.0,50, TimeUnit.MILLISECONDS);
 
         SocketAddress address1 = new InetSocketAddress("127.0.0.1",1000);
         SocketAddress address2 = new InetSocketAddress("127.0.0.1",1001);
         RxSocket<DAPacket> socket1 = router.buildSocket(address1).stack(layers);
+
         RxSocket<DAPacket> socket2 = router.buildSocket(address2).stack(layers);
+
+
+        List<MessageContent> contents = IntStream.range(0,100)
+                .mapToObj(x -> MessageContent.Message(x, 100+x)).collect(Collectors.toList());
+        Set<String> msgSet = contents.stream().map(MessageContent::toString).collect(Collectors.toSet());
 
         // Create TestObservers
         TestObserver<String> test = socket2.upPipe
-                .map(x -> x.getContent().toString()).take(3).test();
+                .map(x -> x.getContent().toString()).take(msgSet.size()).test();
 
-        MessageContent msg1 = MessageContent.Message(12,12);
-        MessageContent msg2 = MessageContent.Message(13,17);
-        MessageContent msg3 = MessageContent.Message(14,128);
-        Set<String> messages = Stream.of(msg1, msg2, msg3).map(MessageContent::toString).collect(Collectors.toSet());
+        //socket2.upPipe.forEach(x -> System.out.println(x.getContent().toString()));
+        Observable.interval(10, TimeUnit.MILLISECONDS)
+                .zipWith(contents, (a, b) -> b)
+                .map(c -> new DAPacket(address2,c))
+                .forEach(socket1.downPipe::onNext);
 
-        socket1.downPipe.onNext(new DAPacket(address2, msg1));
-        socket1.downPipe.onNext(new DAPacket(address2, msg2));
-        socket1.downPipe.onNext(new DAPacket(address2, msg3));
-
-        test.awaitDone(60, TimeUnit.SECONDS)
-                .assertComplete()
-                .assertValueCount(3)
-                .assertValueSet(messages);
+        test.awaitDone(10, TimeUnit.SECONDS)
+                .assertValueCount(msgSet.size())
+                .assertValueSet(msgSet);
     }
 
 }
