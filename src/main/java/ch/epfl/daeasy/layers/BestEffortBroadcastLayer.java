a6f42a1fcdb97e367199c34940d142ea2fd63644
Zyfarok <zyfarok@gmail.com>
2018-11-06 00:39:18 +0100

    Revert "Subtle modification to BEB"
    This reverts commit 25f4c115117a7ba090ad8a442ea931ec905dfe26.

--- src/main/java/ch/epfl/daeasy/layers/BestEffortBroadcastLayer.java
+++ src/main/java/ch/epfl/daeasy/layers/BestEffortBroadcastLayer.java
@@ -1,23 +1,22 @@
 package ch.epfl.daeasy.layers;
 
 import ch.epfl.daeasy.config.Configuration;
+import ch.epfl.daeasy.config.Process;
 import ch.epfl.daeasy.protocol.DAPacket;
 import ch.epfl.daeasy.rxlayers.RxLayer;
 import ch.epfl.daeasy.rxsockets.RxSocket;
 import io.reactivex.Observable;
 
-import java.net.InetSocketAddress;
 import java.util.stream.Collectors;
 
 public class BestEffortBroadcastLayer extends RxLayer<DAPacket, DAPacket> {
 
-    private final Observable<InetSocketAddress> otherProcessesAddresses;
+    private final Observable<Process> otherProcesses;
 
     public BestEffortBroadcastLayer(Configuration cfg) {
-        this.otherProcessesAddresses = Observable.fromIterable(
+        this.otherProcesses = Observable.fromIterable(
                 cfg.processesByPID.values().stream()
                         .filter(p -> p.getPID() != cfg.id)
-                        .map(p -> p.address)
                         .collect(Collectors.toList())
         );
     }
@@ -28,10 +27,8 @@ public class BestEffortBroadcastLayer extends RxLayer<DAPacket, DAPacket> {
     public RxSocket<DAPacket> stackOn(RxSocket<DAPacket> subSocket) {
         RxSocket<DAPacket> socket = new RxSocket<>(subSocket.upPipe);
 
-        socket.downPipe.flatMap(m -> otherProcessesAddresses
-                //.zipWith(Observable.interval(1, TimeUnit.MILLISECONDS),(address, i) -> address)
-                .map(address -> new DAPacket(address, m.getContent()))
-        ).subscribe(subSocket.downPipe);
+        socket.downPipe.flatMap(m -> otherProcesses.map(p -> new DAPacket(p.address, m.getContent())))
+                .subscribe(subSocket.downPipe);
 
         return socket;
     }
