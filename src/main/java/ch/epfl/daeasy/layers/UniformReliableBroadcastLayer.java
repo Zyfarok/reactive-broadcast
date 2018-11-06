851930c6534db08a8099f07d32f5745f0b612a77
Zyfarok <zyfarok@gmail.com>
2018-11-06 16:08:30 +0100

    Massive URB simplification/rewrite

--- src/main/java/ch/epfl/daeasy/layers/UniformReliableBroadcastLayer.java
+++ src/main/java/ch/epfl/daeasy/layers/UniformReliableBroadcastLayer.java
@@ -5,15 +5,18 @@ import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Map;
 import java.util.Set;
+import java.util.concurrent.atomic.AtomicInteger;
 
 import ch.epfl.daeasy.config.Configuration;
 import ch.epfl.daeasy.config.Process;
 import ch.epfl.daeasy.logging.Logging;
 import ch.epfl.daeasy.protocol.DAPacket;
+import ch.epfl.daeasy.protocol.DatagramPacketConverter;
 import ch.epfl.daeasy.protocol.MessageContent;
 import ch.epfl.daeasy.rxlayers.RxLayer;
 import ch.epfl.daeasy.rxsockets.RxSocket;
 import io.reactivex.Observable;
+import io.reactivex.observables.GroupedObservable;
 import io.reactivex.subjects.PublishSubject;
 import io.reactivex.subjects.Subject;
 
@@ -36,103 +39,30 @@ public class UniformReliableBroadcastLayer extends RxLayer<DAPacket, DAPacket> {
      */
     public RxSocket<DAPacket> stackOn(RxSocket<DAPacket> subSocket) {
 
-        Subject<DAPacket> subject = PublishSubject.create();
+        PublishSubject<DAPacket> downPipe = PublishSubject.create();
 
-        RxSocket<DAPacket> socket = new RxSocket<>(subject);
+        // group incoming packets and outgoing packet per MessageContent
+        // Each GroupedObservable is equivalent to an element in pending
+        // The DAPackets that goes through the GroupedObservable serves as acks.
+        Observable<GroupedObservable<MessageContent, DAPacket>> groupedPendingAndAck =
+                subSocket.upPipe.mergeWith(downPipe).groupBy(DAPacket::getContent);
 
-        Subject<DAPacket> intOut = subject;
-        Observable<DAPacket> intIn = socket.downPipe;
-        Observable<DAPacket> extIn = subSocket.upPipe;
-        Subject<DAPacket> extOut = subSocket.downPipe;
+        Observable<DAPacket> delivered = groupedPendingAndAck.flatMap(o -> {
+            // Broadcast any message when we see it for the first time
+            subSocket.downPipe.onNext(new DAPacket(null, o.getKey()));
 
-        // Exterior Messages
-        Observable<DAPacket> messagesExt = extIn.filter(pkt -> pkt.getContent().isMessage());
+            // Create an ack counter
+            AtomicInteger ackCount = new AtomicInteger(N / 2);
 
-        // Interior Messages
-        Observable<DAPacket> messagesIn = intIn;
+            // Count any packet that is not coming from us as an ack
+            // let ONE packet pass once we reach the required amount of acks.
+            return o.filter(
+                    x -> x.getPeer() != null
+                            && !x.getPeer().equals(process.address))
+                    .filter(x -> ackCount.decrementAndGet() == 0);
 
-        // upon event <init> do
-        // delivered := ∅
-        // pending := ∅
-        // forall m do ack [m] := ∅
-        Set<MessageContent> delivered = new HashSet<>();
-        Set<MessageContent> pending = new HashSet<>();
-        Map<MessageContent, Set<Long>> ack = new HashMap<>(); // message -> set of process (ids) that acked the message
-
-        // upon event <urbBroadcast, m> do
-        // pending := pending U {[self,m]}
-        // trigger <bebBroadcast, [Data,self,m]>
-        messagesIn.subscribe(pkt -> {
-            MessageContent m = pkt.getContent();
-            synchronized (pending) {
-                pending.add(m);
-            }
-            extOut.onNext(pkt);
-        }, error -> {
-            Logging.debug("error while receiving message from interior at URB: ");
-            error.printStackTrace();
         });
 
-        // upon event <bebDeliver, pi, [Data,pj,m]> do
-        // ack[m] := ack[m] U {pi}
-        // if [pj,m] not in pending then
-        // pending := pending U {[pj,m]}
-        // trigger <bebBroadcast,[Data,pj,m]>
-        messagesExt.subscribe(pkt -> {
-            MessageContent m = pkt.getContent();
-
-            // // /!\ check if m in delivered
-            // if (delivered.contains(m)) {
-            // return;
-            // }
-
-            if (!this.processesByAddress.containsKey(pkt.getPeer())) {
-                throw new RuntimeException(
-                        "unknown process relay of packet: " + pkt.toString() + "\n" + this.processesByAddress.keySet());
-            }
-
-            // relay is the process that sent us the message, not the origin of the message
-            long relayPID = this.processesByAddress.get(pkt.getPeer()).getPID();
-
-            synchronized (ack) {
-                ack.putIfAbsent(m, new HashSet<Long>());
-                ack.get(m).add(relayPID);
-            }
-
-            synchronized (pending) {
-                if (!pending.contains(m)) {
-                    pending.add(m);
-                    extOut.onNext(new DAPacket(this.process.address, m));
-                }
-            }
-            // upon exists (s, m) ∈ pending such that candeliver (m) ∧ m ∈ delivered do
-            // delivered := delivered ∪ {m} ;
-            // trigger <urb, Deliver | s , m> ;
-
-            synchronized (pending) {
-
-                for (MessageContent smsg : pending) {
-
-                    synchronized (ack) {
-                        if (delivered.contains(smsg)) {
-                            continue;
-                        }
-                        if (ack.getOrDefault(smsg, new HashSet<>()).size() >= (this.N) / 2) {
-                            delivered.add(smsg);
-                            intOut.onNext(pkt);
-
-                            // // /!\ remove m of pending
-                            // pending.remove(smsg);
-                        }
-                    }
-                }
-            }
-
-        }, error -> {
-            Logging.debug("error while receiving message from exterior at URB: " + error.toString());
-            error.printStackTrace();
-        });
-
-        return socket;
+        return new RxSocket<>(delivered, downPipe);
     }
 }
