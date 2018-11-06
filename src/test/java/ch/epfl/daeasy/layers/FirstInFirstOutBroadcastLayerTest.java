8e722e263d694ee29dbdf2c45356336fd4162e89
Zyfarok <zyfarok@gmail.com>
2018-11-06 16:08:59 +0100

    Remove groupedLayer from Fifo tests

--- src/test/java/ch/epfl/daeasy/layers/FirstInFirstOutBroadcastLayerTest.java
+++ src/test/java/ch/epfl/daeasy/layers/FirstInFirstOutBroadcastLayerTest.java
@@ -58,7 +58,8 @@ public class FirstInFirstOutBroadcastLayerTest {
                 final DatagramPacketConverter daConverter = new DatagramPacketConverter();
                 final RxLayer<DatagramPacket, DAPacket> perfectLinks = new RxNil<DatagramPacket>()
                         .convertPipes(daConverter)
-                        .stack(RxGroupedLayer.create(x -> x.getPeer().toString(), perfectLinkLayer));
+                        //.stack(RxGroupedLayer.create(x -> x.getPeer().toString(), perfectLinkLayer));
+                        .stack(perfectLinkLayer);
 
                 final RxLayer<DatagramPacket, DAPacket> fifo = perfectLinks
                         .stack(new BestEffortBroadcastLayer(cfgs.get(i)))
