3ad2c6613ba79c78e6f81b18f9691245f4a7d018
Pierre Thévenet <thevenet.pierre@gmail.com>
2018-11-29 23:23:01 +0100

    LCB tests update

--- src/test/java/ch/epfl/daeasy/layers/LocalCausalBroadcastLayerLivenessTest.java
+++ src/test/java/ch/epfl/daeasy/layers/LocalCausalBroadcastLayerLivenessTest.java
@@ -6,22 +6,26 @@ import java.net.DatagramPacket;
 import java.net.InetSocketAddress;
 import java.net.SocketAddress;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 import java.util.concurrent.TimeUnit;
 
-import ch.epfl.daeasy.protocol.*;
 import org.junit.Test;
 
-import ch.epfl.daeasy.config.Configuration;
 import ch.epfl.daeasy.config.LCBConfiguration;
+import ch.epfl.daeasy.protocol.CausalDatagramPacketConverter;
+import ch.epfl.daeasy.protocol.CausalMessageContent;
+import ch.epfl.daeasy.protocol.DAPacket;
+import ch.epfl.daeasy.protocol.MessageContent;
 import ch.epfl.daeasy.rxlayers.RxFilterLayer;
 import ch.epfl.daeasy.rxlayers.RxLayer;
 import ch.epfl.daeasy.rxlayers.RxNil;
 import ch.epfl.daeasy.rxsockets.RxBadRouter;
 import ch.epfl.daeasy.rxsockets.RxClosableSocket;
 import ch.epfl.daeasy.rxsockets.RxSocket;
+import io.reactivex.Observable;
 import io.reactivex.observers.TestObserver;
 
 public class LocalCausalBroadcastLayerLivenessTest {
@@ -47,13 +51,12 @@ public class LocalCausalBroadcastLayerLivenessTest {
                 cfgs.add(new LCBConfiguration(i + 1, "test/membership_LCB_3p_liveness_test.txt", 1));
                 addrs.add(new InetSocketAddress("127.0.0.1", 10001 + i));
 
-                RxLayer<DAPacket<CausalMessageContent>, DAPacket<CausalMessageContent>> perfectLinkLayer = new PerfectLinkLayer<>(CausalMessageContent::toAck);
+                RxLayer<DAPacket<CausalMessageContent>, DAPacket<CausalMessageContent>> perfectLinkLayer = new PerfectLinkLayer<>(
+                        CausalMessageContent::toAck);
                 if (i == 0) {
-                    // filter: p1 does not receive messages from p1 with pid = 3
+                    // filter: p1 does not receive messages with pid=3
                     // filter just above the perfect link : beb will never deliver those messages
-                    perfectLinkLayer = perfectLinkLayer.stack(new RxFilterLayer<>(x -> true, x -> true)); // x.getContent().getPID()
-                                                                                                                  // !=
-                                                                                                                  // 3
+                    perfectLinkLayer = perfectLinkLayer.stack(new RxFilterLayer<>(x -> x.content.pid != 3, x -> true));
                 }
                 final CausalDatagramPacketConverter daConverter = new CausalDatagramPacketConverter();
                 final RxLayer<DatagramPacket, DAPacket<CausalMessageContent>> perfectLinks = new RxNil<DatagramPacket>()
@@ -62,7 +65,8 @@ public class LocalCausalBroadcastLayerLivenessTest {
                 final RxLayer<DatagramPacket, DAPacket<CausalMessageContent>> beb = perfectLinks
                         .stack(new BestEffortBroadcastLayer<>(cfgs.get(i)));
 
-                final RxLayer<DatagramPacket, CausalMessageContent> urb = beb.stack(new UniformReliableBroadcastLayer<>(cfgs.get(i)));
+                final RxLayer<DatagramPacket, CausalMessageContent> urb = beb
+                        .stack(new UniformReliableBroadcastLayer<>(cfgs.get(i)));
                 final RxLayer<DatagramPacket, MessageContent> lcb = urb
                         .stack(new LocalizedCausalBroadcastLayer(cfgs.get(i)));
 
@@ -88,19 +92,25 @@ public class LocalCausalBroadcastLayerLivenessTest {
 
         int messageCount = 2;
         try {
+            Observable<MessageContent> upPipe1 = sockets.get(0).upPipe.share();
+            Observable<MessageContent> upPipe2 = sockets.get(1).upPipe.share();
+            Observable<MessageContent> upPipe3 = sockets.get(2).upPipe.share();
+
             // Create TestObservers
-            TestObserver<String> test1 = sockets.get(0).upPipe.map(MessageContent::toString).take(messageCount)
-                    .test();
-            TestObserver<String> test2 = sockets.get(1).upPipe.map(MessageContent::toString).take(messageCount)
-                    .test();
-            TestObserver<String> test3 = sockets.get(2).upPipe.map(MessageContent::toString).take(messageCount)
-                    .test();
+            TestObserver<String> test1 = upPipe1.map(MessageContent::toString).take(messageCount).test();
+            TestObserver<String> test2 = upPipe2.map(MessageContent::toString).take(messageCount).test();
+            TestObserver<String> test3 = upPipe3.map(MessageContent::toString).take(messageCount).test();
 
             MessageContent m1 = MessageContent.createMessage(3, 1);
             MessageContent m2 = MessageContent.createMessage(2, 1);
 
+            Set<String> messages = new HashSet<>(Arrays.asList(m1.toString(), m2.toString()));
+
             Set<String> deliveredMessages = new HashSet<>();
             deliveredMessages.add(m2.toString());
+
+            // p2 sends m2 once it delivered m1
+            upPipe2.subscribe(x -> sockets.get(1).downPipe.onNext(m2));
 
             // p3 sends m1
             sockets.get(2).downPipe.onNext(m1);
@@ -108,20 +118,18 @@ public class LocalCausalBroadcastLayerLivenessTest {
             // p2 should receive m1
             Thread.sleep(500);
 
-            // p2 sends m2
-            sockets.get(1).downPipe.onNext(m2);
-
             // p3 and p1 should receive m2
             Thread.sleep(500);
 
-            // since p1 does not receive m1, all processes should deliver m2 but not m1
+            // p1 should have delivered m2 but not m1
             test1.awaitDone(5, TimeUnit.SECONDS).assertValueCount(1).assertValueSet(deliveredMessages);
-            test2.awaitDone(5, TimeUnit.SECONDS).assertValueCount(1).assertValueSet(deliveredMessages);
-            test3.awaitDone(5, TimeUnit.SECONDS).assertValueCount(1).assertValueSet(deliveredMessages);
 
+            // other processes should have delivered all two messages
+            test1.awaitDone(5, TimeUnit.SECONDS).assertValueCount(messageCount).assertValueSet(messages);
+            test1.awaitDone(5, TimeUnit.SECONDS).assertValueCount(messageCount).assertValueSet(messages);
         } catch (Exception e) {
             e.printStackTrace();
-            fail("exception: " + e.toString());
         }
+
     }
 }
--- src/test/java/ch/epfl/daeasy/layers/LocalCausalBroadcastLayerSafetyTest.java
+++ src/test/java/ch/epfl/daeasy/layers/LocalCausalBroadcastLayerSafetyTest.java
@@ -9,11 +9,13 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.TimeUnit;
 
-import ch.epfl.daeasy.protocol.*;
 import org.junit.Test;
 
-import ch.epfl.daeasy.config.Configuration;
 import ch.epfl.daeasy.config.LCBConfiguration;
+import ch.epfl.daeasy.protocol.CausalDatagramPacketConverter;
+import ch.epfl.daeasy.protocol.CausalMessageContent;
+import ch.epfl.daeasy.protocol.DAPacket;
+import ch.epfl.daeasy.protocol.MessageContent;
 import ch.epfl.daeasy.rxlayers.RxFilterLayer;
 import ch.epfl.daeasy.rxlayers.RxLayer;
 import ch.epfl.daeasy.rxlayers.RxNil;
@@ -21,6 +23,7 @@ import ch.epfl.daeasy.rxsockets.RxBadRouter;
 import ch.epfl.daeasy.rxsockets.RxClosableSocket;
 import ch.epfl.daeasy.rxsockets.RxSocket;
 import io.reactivex.observers.TestObserver;
+import io.reactivex.Observable;
 
 public class LocalCausalBroadcastLayerSafetyTest {
 
@@ -46,13 +49,12 @@ public class LocalCausalBroadcastLayerSafetyTest {
                 cfgs.add(new LCBConfiguration(i + 1, "test/membership_LCB_3p_safety_test.txt", 1));
                 addrs.add(new InetSocketAddress("127.0.0.1", 10001 + i));
 
-                RxLayer<DAPacket<CausalMessageContent>, DAPacket<CausalMessageContent>> perfectLinkLayer =
-                        new PerfectLinkLayer<>(CausalMessageContent::toAck);
+                RxLayer<DAPacket<CausalMessageContent>, DAPacket<CausalMessageContent>> perfectLinkLayer = new PerfectLinkLayer<>(
+                        CausalMessageContent::toAck);
                 if (i == 2) {
-                    // filter: p3 does not receive messages from p1 with pid = 1
+                    // filter: p3 does not receive messages with pid = 1
                     // filter just above the perfect link : beb will never deliver those messages
-                    perfectLinkLayer = perfectLinkLayer
-                            .stack(new RxFilterLayer<>(x -> x.content.pid != 1, x -> true));
+                    perfectLinkLayer = perfectLinkLayer.stack(new RxFilterLayer<>(x -> x.content.pid != 1, x -> true));
                 }
                 final CausalDatagramPacketConverter daConverter = new CausalDatagramPacketConverter();
                 final RxLayer<DatagramPacket, DAPacket<CausalMessageContent>> perfectLinks = new RxNil<DatagramPacket>()
@@ -61,8 +63,8 @@ public class LocalCausalBroadcastLayerSafetyTest {
                 final RxLayer<DatagramPacket, DAPacket<CausalMessageContent>> beb = perfectLinks
                         .stack(new BestEffortBroadcastLayer<>(cfgs.get(i)));
 
-                final RxLayer<DatagramPacket, CausalMessageContent> urb =
-                        beb.stack(new UniformReliableBroadcastLayer<>(cfgs.get(i)));
+                final RxLayer<DatagramPacket, CausalMessageContent> urb = beb
+                        .stack(new UniformReliableBroadcastLayer<>(cfgs.get(i)));
                 final RxLayer<DatagramPacket, MessageContent> lcb = urb
                         .stack(new LocalizedCausalBroadcastLayer(cfgs.get(i)));
 
@@ -90,16 +92,21 @@ public class LocalCausalBroadcastLayerSafetyTest {
 
         int messageCount = 2;
         try {
+
+            Observable<MessageContent> upPipe1 = sockets.get(0).upPipe.share();
+            Observable<MessageContent> upPipe2 = sockets.get(1).upPipe.share();
+            Observable<MessageContent> upPipe3 = sockets.get(2).upPipe.share();
+
             // Create TestObservers
-            TestObserver<String> test1 = sockets.get(0).upPipe.map(MessageContent::toString).take(messageCount)
-                    .test();
-            TestObserver<String> test2 = sockets.get(1).upPipe.map(MessageContent::toString).take(messageCount)
-                    .test();
-            TestObserver<String> test3 = sockets.get(2).upPipe.map(MessageContent::toString).take(messageCount)
-                    .test();
+            TestObserver<String> test1 = upPipe1.map(MessageContent::toString).take(messageCount).test();
+            TestObserver<String> test2 = upPipe2.map(MessageContent::toString).take(messageCount).test();
+            TestObserver<String> test3 = upPipe3.map(MessageContent::toString).take(messageCount).test();
 
             // stop p3 for now
             closables.get(2).close();
+
+            // p2 sends m2 once it delivered m1
+            upPipe2.subscribe(x -> sockets.get(1).downPipe.onNext(MessageContent.createMessage(2, 1)));
 
             // p1 sends m1
             sockets.get(0).downPipe.onNext(MessageContent.createMessage(1, 1));
@@ -109,9 +116,6 @@ public class LocalCausalBroadcastLayerSafetyTest {
 
             // stop p1
             closables.get(0).close();
-
-            // p2 sends m2
-            sockets.get(1).downPipe.onNext(MessageContent.createMessage(2,1));
 
             Thread.sleep(500);
 
@@ -126,14 +130,8 @@ public class LocalCausalBroadcastLayerSafetyTest {
 
             Thread.sleep(1000);
 
-            // p3 should have received m2 from p1 and p2, but m1 only from p2
+            // p3 should have received m2 from p1 and p2, but not m1 from any p1 or p2
             // thus since m1 -> m2, p3 should not have delivered anything
-            // p2 should have delivered no messages as well (m1 -> m2 and p3 does not ack
-            // m1)
-            // p1 should not have delivered any messages since m1 -> n2 and p3 does not ack
-            // m1
-            test1.awaitDone(5, TimeUnit.SECONDS).assertValueCount(0);
-            test2.awaitDone(5, TimeUnit.SECONDS).assertValueCount(0);
             test3.awaitDone(5, TimeUnit.SECONDS).assertValueCount(0);
 
         } catch (Exception e) {
