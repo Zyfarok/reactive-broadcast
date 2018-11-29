85663aa4a98c3a660b290c1cb6a5e175f4ab762a
Zyfarok <zyfarok@gmail.com>
2018-11-29 17:20:32 +0100

    Improve LCB

--- src/main/java/ch/epfl/daeasy/layers/LocalizedCausalBroadcastLayer.java
+++ src/main/java/ch/epfl/daeasy/layers/LocalizedCausalBroadcastLayer.java
@@ -1,18 +1,16 @@
 package ch.epfl.daeasy.layers;
 
-import ch.epfl.daeasy.config.Configuration;
 import ch.epfl.daeasy.config.LCBConfiguration;
-import ch.epfl.daeasy.config.Process;
 import ch.epfl.daeasy.protocol.CausalMessageContent;
 import ch.epfl.daeasy.protocol.MessageContent;
 import ch.epfl.daeasy.rxlayers.RxLayer;
 import ch.epfl.daeasy.rxsockets.RxSocket;
+import com.google.common.collect.ImmutableMap;
 import com.google.common.collect.ImmutableSet;
 import io.reactivex.Observable;
 import io.reactivex.subjects.BehaviorSubject;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.util.concurrent.atomic.AtomicLong;
 import java.util.stream.Collectors;
 
 public class LocalizedCausalBroadcastLayer extends RxLayer<CausalMessageContent, MessageContent> {
@@ -28,31 +26,34 @@ public class LocalizedCausalBroadcastLayer extends RxLayer<CausalMessageContent,
      * Assumes subSocket is a UniformReliableBroadcast
      */
     public RxSocket<MessageContent> stackOn(RxSocket<CausalMessageContent> subSocket) {
-        Map<Long, BehaviorSubject<Long>> lastDelivered = new HashMap<>();
-
         // initialize data structures
-        for (Integer pid : this.cfg.processesByPID.keySet()) {
-            lastDelivered.put(pid.longValue(), BehaviorSubject.createDefault(0L));
+        ImmutableMap.Builder<Long, BehaviorSubject<Long>> deliveryEventsBuilder = ImmutableMap.builder();
+        ImmutableMap.Builder<Long, AtomicLong> lastDeliveredBuilder = ImmutableMap.builder();
+        for(Integer pid : cfg.processesByPID.keySet()) {
+            deliveryEventsBuilder.put(pid.longValue(), BehaviorSubject.createDefault(0L));
+            lastDeliveredBuilder.put(pid.longValue(), new AtomicLong(0L));
         }
+        final ImmutableMap<Long, BehaviorSubject<Long>> deliveryEvents = deliveryEventsBuilder.build();
+        final ImmutableMap<Long, AtomicLong> lastDelivered = lastDeliveredBuilder.build();
 
+        // Process incoming messages
         Observable<MessageContent> upPipe = subSocket.upPipe.flatMap(cmc -> // For each message
                 Observable.fromIterable(cmc.causes).flatMap(c -> // For each clause
-                        lastDelivered.get(c.pid) // Check the deliver status
+                        deliveryEvents.get(c.pid) // Check the deliver status
                                 .filter(seq -> seq >= c.seq).take(1) // And wait until the status is valid
                 ).takeLast(1) // Wait until all causes are satisfied
                 .map(x -> cmc.withoutCauses()) // And then deliver the message
-        ).map(mc -> {
-            // When any message is delivered, notify others.
-            lastDelivered.get(mc.pid).onNext(mc.seq);
-            return mc;
-        });
+        ).doOnNext(mc -> lastDelivered.get(mc.pid).incrementAndGet()).share();
+
+        // Notify others about the delivery
+        upPipe.forEach(mc -> deliveryEvents.get(mc.pid).onNext(mc.seq));
 
         RxSocket<MessageContent> socket = new RxSocket<>(upPipe);
 
         // Add causes to down-going MessageContents
         socket.downPipe.map(mc -> mc.withCauses(
                 dependencies.stream()
-                        .map(pid -> new CausalMessageContent.Cause(pid, lastDelivered.get(pid.longValue()).getValue()))
+                        .map(pid -> new CausalMessageContent.Cause(pid, lastDelivered.get(pid.longValue()).get()))
                         .collect(Collectors.toList())
         )).subscribe(subSocket.downPipe);
 
